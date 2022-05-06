79c7f88725f6c3c23ba04b5f445e46674810726d
wangx512 <wangx512@newschool.edu>
2022-05-06 03:26:09 -0400

    4

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-var uk = document.querySelectorAll("#uk li");
-var sign = document.querySelector("#sign");
-var lastItem = document.querySelector("#uk li.last");
-
-lastItem.addEventListener("animationend", function () {
-  text();
-});
-console.log(uk.length);
-
-function classAddRemove(el, i) {
-  el.classList.remove("active");
-  setTimeout(function () {
-    el.classList.add("active");
-  }, i * 600);
-}
-
-function text() {
-  uk.forEach((el, index) => {
-    el.classList.remove("active");
-    setTimeout(() => {
-      el.classList.add("active");
-    }, index * 600);
+$(document).mousemove(function (e) {
+    $(".pointer").css({ left: e.pageX, top: e.pageY });
   });
-}
-
-text();
